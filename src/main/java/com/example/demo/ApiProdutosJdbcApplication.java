4245f9195269cd9cd5e30f51d3cc15eb9903961f
samuelmsilva2v <samuelmsilva@outlook.com.br>
2025-02-14 15:15:38 -0300

    first commit

---
+++ src/main/java/com/example/demo/ApiProdutosJdbcApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiProdutosJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiProdutosJdbcApplication.class, args);
+	}
+
+}
