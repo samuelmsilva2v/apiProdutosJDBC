d46735a7c9ab2c1d65c2aeaa437fd6db991fb8e8
samuelmsilva2v <samuelmsilva@outlook.com.br>
2025-02-14 15:33:20 -0300

    docker-compose, script sql

---
+++ src/main/java/com/example/demo/sql/script.sql
@@ -0,0 +1,23 @@
+CREATE TABLE categoria(
+	id			uuid			PRIMARY KEY,
+	nome		VARCHAR(50)		NOT NULL UNIQUE
+);
+
+CREATE TABLE produto(
+	id				uuid			PRIMARY KEY,
+	nome			VARCHAR(100)	NOT NULL,
+	preco			DECIMAL(10,2)	NOT NULL,
+	quantidade		INT				NOT NULL,
+	categoria_id	uuid			NOT NULL,
+	FOREIGN KEY(categoria_id)
+	REFERENCES categoria(id)
+);
+
+INSERT INTO categoria(id, nome) VALUES(gen_random_uuid(), 'Informática');
+INSERT INTO categoria(id, nome) VALUES(gen_random_uuid(), 'Eletrônicos');
+INSERT INTO categoria(id, nome) VALUES(gen_random_uuid(), 'Livraria');
+INSERT INTO categoria(id, nome) VALUES(gen_random_uuid(), 'Vestuário');
+INSERT INTO categoria(id, nome) VALUES(gen_random_uuid(), 'Outros');
+
+SELECT * FROM categoria;
+
