85c3a549b2156a74313f818f7cf13514698ead2a
samuelmsilva2v <samuelmsilva@outlook.com.br>
2025-02-18 13:56:08 -0300

    categoria repository, categoria controller

---
+++ src/main/java/com/example/demo/repositories/CategoriaRepository.java
@@ -0,0 +1,41 @@
+package com.example.demo.repositories;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import com.example.demo.entities.Categoria;
+import com.example.demo.factories.ConnectionFactory;
+
+public class CategoriaRepository {
+
+	private ConnectionFactory connectionFactory = new ConnectionFactory();
+
+	public List<Categoria> findAll() {
+
+		try {
+
+			var connection = connectionFactory.getConnection();
+			var statement = connection.createStatement();
+			var result = statement.executeQuery("SELECT * FROM categorias");
+
+			var lista = new ArrayList<Categoria>();
+
+			while (result.next()) {
+
+				var categoria = new Categoria();
+				categoria.setId(UUID.fromString(result.getString("id")));
+				categoria.setNome(result.getString("nome"));
+
+				lista.add(categoria);
+			}
+
+			connection.close();
+			return lista;
+		} 
+		catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+}
