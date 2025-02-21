062e2ea693a04fd863c362622955f0ad3476e6c0
samuelmsilva2v <samuelmsilva@outlook.com.br>
2025-02-21 13:39:05 -0300

    produto controller

--- src/main/java/com/example/demo/controllers/ProdutoController.java
+++ src/main/java/com/example/demo/controllers/ProdutoController.java
@@ -1,6 +1,10 @@
 package com.example.demo.controllers;
 
+import java.util.List;
+import java.util.UUID;
+
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -8,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.dtos.ProdutoRequestDto;
+import com.example.demo.entities.Produto;
 import com.example.demo.services.ProdutoService;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -27,19 +32,25 @@ public class ProdutoController {
 	
 	@Operation(summary = "Serviço para atualizar um produto.")
 	@PutMapping("atualizar")
-	public void atualizarProduto() {
-		// TODO
+	public String atualizarProduto(@RequestBody @Valid ProdutoRequestDto request, @PathVariable UUID id) {
+		return produtoService.atualizarProduto(request, id);
 	}
 	
 	@Operation(summary = "Serviço para excluir um produto.")
 	@DeleteMapping("excluir")
-	public void excluirProduto() {
-		// TODO
+	public String excluirProduto(@PathVariable UUID id) {
+		return produtoService.excluirProduto(id);
 	}
 	
 	@Operation(summary = "Serviço para consultar todos os produtos.")
 	@PostMapping("consultar")
-	public void consultarProdutos() {
-		// TODO
+	public List<Produto> consultarProdutos() {
+		return produtoService.consultarProdutos();
+	}
+	
+	@Operation(summary = "Serviço para consultar produtos por nome.")
+	@PostMapping("consultar/{nome}")
+	public List<Produto> consultarProdutosPorNome(@PathVariable String nome) {
+		return produtoService.consultarProdutosPorNome(nome);
 	}
 }
--- src/main/java/com/example/demo/entities/Produto.java
+++ src/main/java/com/example/demo/entities/Produto.java
@@ -9,7 +9,6 @@ public class Produto {
 
 	private UUID id;
 	private String nome;
-	private String descricao;
 	private Double preco;
 	private Integer quantidade;
 	private Categoria categoria;
